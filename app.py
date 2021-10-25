ed292fc68ab0a9c1c957a51131865ead8aedcc84
mGolos <mathieu.golos@gmail.com>
2021-10-25 16:43:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     st.sidebar.image('files/logo.png')
     st.markdown("<style>.css-ng1t4o {padding: 1rem 1rem;} .css-12oz5g7 {padding: 1rem 1rem;}</style>", unsafe_allow_html=True)
 
-    shortpage = labmda: st.write("[Also available on AWS.](http://3.138.183.205:8501)")
+    shortpage = lambda: st.subheader("[Also available on AWS.](http://3.138.183.205:8501)")
     subpages.utils.st_query_radio("Menu", "page", {
         "Chessboard": ('Task1 Chessboard', subpages.chessboard),
         'Today': ("Task2 Today", subpages.today),
